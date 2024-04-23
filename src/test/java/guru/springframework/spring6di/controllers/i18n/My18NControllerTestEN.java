f6d9596fd1353cc83cf2e33bbfbbe76556e4fcf2
Camille BERNARD <cabernard@klanik.com>
2024-04-23 13:28:22 +0200

    Using default profile EN

--- src/test/java/guru/springframework/spring6di/controllers/i18n/My18NControllerTestEN.java
+++ src/test/java/guru/springframework/spring6di/controllers/i18n/My18NControllerTestEN.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@ActiveProfiles("EN")
+//ActiveProfiles("EN")
 public class My18NControllerTestEN {
 
     @Autowired
